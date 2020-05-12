4c6651dd4312055bd8100229ac411b241eb52868
Abdul Khan <akhan17@outlook.com>
2020-05-12 23:58:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,7 +3,7 @@ class Student
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
-    new_student = self.new
+    new_student = self.class.new
 
     new_student = row[0]
     new_student = row[1]
