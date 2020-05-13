9138e85b4cf35ca65dcab00a541d34d3addc93ec
Abdul Khan <akhan17@outlook.com>
2020-05-13 00:11:09 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -54,6 +54,9 @@ class Student
 
   def self.students_below_12th_grade
     sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE students.grade < 12
     SQL
 
     DB[:conn].execute(sql).collect do |row|
